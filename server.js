86cdb2e05bd187ddf9dd4c92203d7755c8b7f38a
Ramiz khan <ramizkhan@Ramizs-MacBook-Pro-2.local>
2017-12-18 20:51:17 -0600

    hku works

--- server.js
+++ server.js
@@ -29,8 +29,10 @@ mongoose.Promise = Promise;
 
 if (process.env.MONGODB_URI) {
 
-  mongoose.connect("process.env.MONGODB_URI")
-
+  mongoose.connect("process.env.MONGODB_URI", {
+    useMongoClient: true
+  });
+  
 }
  else {
 mongoose.connect("mongodb://localhost/Scraper_DB", {
