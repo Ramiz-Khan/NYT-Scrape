fffba179ff2d232169bc7bb79cee7f8bdc401e3a
Ramiz khan <ramizkhan@Ramizs-MacBook-Pro-2.local>
2017-12-18 20:46:03 -0600

    hku works

--- server.js
+++ server.js
@@ -25,11 +25,18 @@ app.use(express.static("public"));
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
+
+if (process.env.MONGODB_URI) {
+
+  mongoose.connect("process.env.MONGODB_URI"
+
+}
+ else {
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/Scraper_DB", {
   useMongoClient: true
 });
-
+ }
 // Routes
 
 // A GET route for scraping the Dallas Morning News website
